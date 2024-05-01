f09f05079efa0bbb5266cfec4d76a44081309fad
Giuseppe Daidone <65767930+GiuseppeDaidone@users.noreply.github.com>
2024-05-01 13:13:00 +0200

    Updated version

--- src/main/java/laboratory/flicktime/FlickMovieApplication.java
+++ src/main/java/laboratory/flicktime/FlickMovieApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SiwMovie03Application {
+public class FlickMovieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SiwMovie03Application.class, args);
+		SpringApplication.run(FlickMovieApplication.class, args);
 	}
+
 }
--- src/test/java/laboratory/flicktime/demo/DemoApplicationTests.java
+++ src/test/java/laboratory/flicktime/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package it.uniroma3.siw.demo;
+package laboratory.flicktime.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
